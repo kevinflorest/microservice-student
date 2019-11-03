439a45d8cd26aff2258c5a4cfa3e889c1900e1d4
Kevin Peter Flores Tarmeño <kflorest@everis.com>
2019-11-03 15:33:08 -0500

    Microservice of Student

--- API-Student/src/test/java/com/sistema/app/ApiStudentApplicationTests.java
+++ API-Student/src/test/java/com/sistema/app/ApiStudentApplicationTests.java
@@ -1,7 +1,6 @@
 package com.sistema.app;
 
 import java.util.Collections;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +8,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.reactive.server.WebTestClient;
-
 import com.sistema.app.models.Student;
 import com.sistema.app.services.StudentService;
 
@@ -23,29 +21,29 @@ public class ApiStudentApplicationTests {
 	@Autowired
 	private StudentService service;
 	
-//	@Test
-//	public void listStudent() {
-//		client.get().uri("/api/student/")
-//		.accept(MediaType.APPLICATION_JSON_UTF8)
-//		.exchange()
-//		.expectStatus().isOk()
-//		.expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8)
-//		.expectBodyList(Student.class);
-//	}
-//	
-//	@Test
-//	public void findByStudent() {
-//		
-//		Student student = service.findByIdStudent("asds").block();
-//		
-//		client.get().uri("/api/student/{id}", Collections.singletonMap("id", student.getId()))
-//		.accept(MediaType.APPLICATION_JSON_UTF8)
-//		.exchange()
-//		.expectStatus().isOk()
-//		.expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8)
-//		.expectBodyList(Student.class);
-//	}
-//	
-//	
+	@Test
+	public void listStudent() {
+		client.get().uri("/api/student/")
+		.accept(MediaType.APPLICATION_JSON_UTF8)
+		.exchange()
+		.expectStatus().isOk()
+		.expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8)
+		.expectBodyList(Student.class);
+	}
+	
+	@Test
+	public void findByStudent() {
+		
+		Student student = service.findByIdStudent("asds").block();
+		
+		client.get().uri("/api/student/{id}", Collections.singletonMap("id", student.getId()))
+		.accept(MediaType.APPLICATION_JSON_UTF8)
+		.exchange()
+		.expectStatus().isOk()
+		.expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8)
+		.expectBodyList(Student.class);
+	}
+	
+	
 
 }
